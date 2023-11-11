c4bee1874326dfc38393610b63f644b722176b59
Cladorhiza <80283120+Cladorhiza@users.noreply.github.com>
2023-11-11 03:19:02 +0000

    now supports mvp, still only wireframe

--- Rasterizer/src/Rasterizer.cpp
+++ Rasterizer/src/Rasterizer.cpp
@@ -2,47 +2,132 @@
 #include "glew.h"
 #include "gl/GL.h"
 #include "glfw3.h"
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
 
 #include <iostream>
 #include <vector>
 
-#include "Shader.h"
+const int WIDTH = 640;
+const int HEIGHT = 480;
+constexpr int FRAMEBUFFER_SIZE = WIDTH * HEIGHT;
 
-unsigned BuildVAOfromData(const std::vector<float>& vertexes, 
-                                const std::vector<float>& colours, 
-                                const std::vector<unsigned>& indexes)
-{
-    //ids for buffer objects
-    unsigned vbo[2], vao, ibo;
+const glm::vec4 black{0.0f, 0.0f, 0.0f, 1.0f};
+const glm::vec4 red{1.0f, 0.0f, 0.0f, 1.0f};
+const glm::vec4 green{0.0f, 1.0f, 0.0f, 1.0f};
+const glm::vec4 blue{0.0f, 0.0f, 1.0f, 1.0f};
+const glm::vec4 yellow{1.0f, 1.0f, 0.0f, 1.0f};
+const glm::vec4 burgundy{0.5f, 0.0f, 0.125f, 1.0f};
 
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+struct Triangle{
 
-    //generate vertex buffers and assign vertexes to first
-    glGenBuffers(2, vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glBufferData(GL_ARRAY_BUFFER, vertexes.size() * sizeof(float), vertexes.data(), GL_STATIC_DRAW);
+    Triangle(){
+    }
 
-    //shader layout location for vertexes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-    glEnableVertexAttribArray(0);
+    Triangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const glm::vec4& colour)
+        : v1(v1), v2(v2), v3(v3), colour(colour)
+    {
 
-    //assign colours to second
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-    glBufferData(GL_ARRAY_BUFFER, colours.size() * sizeof(float), colours.data(), GL_STATIC_DRAW);
+    }
 
-    //shader layout location for colours
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-    glEnableVertexAttribArray(1);
+    glm::vec3 v1;
+    glm::vec3 v2;
+    glm::vec3 v3;
+    glm::vec4 colour;
+};
 
-    //generate index buffer and assign indexes to it
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.size() * sizeof(unsigned), indexes.data(), GL_STATIC_DRAW);
+struct Shader{
+    
+    glm::mat4 proj;
+    glm::mat4 view;
 
-    glBindVertexArray(0);
-    return vao;
-}
+    Shader(const glm::mat4 proj, const glm::mat4 view)
+        :proj(proj), view(view)
+    {
+    
+    }
+
+    Triangle RasterizeTriangle(const Triangle& t, const glm::mat4 model){
+        
+        Triangle raster;
+        glm::mat4 mvp{ proj * view * model };
+        
+        //get in clip space
+        raster.v1 = mvp * glm::vec4{t.v1, 1.0f};
+        raster.v2 = mvp * glm::vec4{t.v2, 1.0f};
+        raster.v3 = mvp * glm::vec4{t.v3, 1.0f};
+
+        //TODO: clip...
+
+        //get in screen space
+        raster.v1.x = (raster.v1.x + 1) * (WIDTH / 2);
+        raster.v1.y = (raster.v1.y + 1) * (HEIGHT / 2);
+        raster.v2.x = (raster.v2.x + 1) * (WIDTH / 2);
+        raster.v2.y = (raster.v2.y + 1) * (HEIGHT / 2);
+        raster.v3.x = (raster.v3.x + 1) * (WIDTH / 2);
+        raster.v3.y = (raster.v3.y + 1) * (HEIGHT / 2);
+
+
+        //TODO: Lerp colours
+        raster.colour = t.colour;
+        return raster;
+    }
+    
+
+};
+
+struct FrameBuffer{
+
+    FrameBuffer(){
+        Colours.reserve(FRAMEBUFFER_SIZE);
+        Depth.reserve(FRAMEBUFFER_SIZE);
+    }
+    
+    std::vector<glm::vec4> Colours;
+    std::vector<float> Depth;
+
+    void Clear(){
+        for (int i{0}; i < Colours.size(); i++){
+            Colours[i] = black;
+        }
+    }
+
+    void DrawLine(const glm::vec2& v1, const glm::vec2& v2, const glm::vec4& colour){
+        
+        float dy{ (v1.y - v2.y) };
+        float dx{ (v1.x - v2.x) };
+        float step;
+
+        if (abs(dx) > abs(dy)){
+            step = abs(dx);
+        }
+        else step = abs(dy);
+        
+        float xInc { dx / step };
+        float yInc { dy / step };
+
+        for (int i{ 0 }; i <= step; i++){
+            int xIndex { static_cast<int>(round(v2.x + (xInc * i))) };
+            int yIndex { static_cast<int>(round(v2.y + (yInc * i))) };
+
+            //TODO: replace this with real clipping, as it could be that coordinates are outside screen but the resulting lines/faces are in the screen
+            if (xIndex < 0 || xIndex >= WIDTH || yIndex < 0 || yIndex >= HEIGHT) continue;
+
+            Colours[xIndex + (yIndex * WIDTH)] = colour;
+        }
+    }
+
+    void DrawTriangle(const Triangle& t){
+        DrawLine(glm::vec2{t.v1.x, t.v1.y}, glm::vec2{t.v2.x, t.v2.y}, t.colour);
+        DrawLine(glm::vec2{t.v2.x, t.v2.y}, glm::vec2{t.v3.x, t.v3.y}, t.colour);
+        DrawLine(glm::vec2{t.v3.x, t.v3.y}, glm::vec2{t.v1.x, t.v1.y}, t.colour);
+
+        //TODO: FILL POLYGON
+    }
+
+    
+
+};
 
 int main(void)
 {
@@ -52,8 +137,9 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    std::cout << "Window width: " << WIDTH << ". Window height: " << HEIGHT << '\n';
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(500, 500, "Rasterizer", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Rasterizer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -71,52 +157,63 @@ int main(void)
     }
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
-    //Shader basicShader("res/shaders/BasicShader.txt");
-    //basicShader.Bind();
+    FrameBuffer frameBuff;
 
-    //glViewport(0,0,640,480);
-
-
-    const float pixelrgbs[] {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f,
-        1.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 1.0f
-    };
-
-    std::vector<float> pixlrgb;
-    pixlrgb.reserve(500 * 500);
-
-    for (int i{0}; i < (500*500); i++){
-        int temp { (i % 5) * 3};
-        pixlrgb.emplace_back(pixelrgbs[temp]);
-        pixlrgb.emplace_back(pixelrgbs[temp + 1]);
-        pixlrgb.emplace_back(pixelrgbs[temp + 2]);
+    for (int i{0}; i < (WIDTH*HEIGHT); i++){
+        frameBuff.Colours.emplace_back(0.0f, 0.0f, 0.0f, 1.0f);
     }
     
-    int width,height;
-    glfwGetWindowSize(window, &width, &height);
-    std::cout << "Window width: " << width << ". Window height: " << height << '\n';
+    glm::mat4 proj { glm::perspective(110.0f, static_cast<float>(WIDTH)/HEIGHT, 0.01f, 10000.0f) };
+    glm::mat4 view { 1.0f };
+    glm::translate(view, glm::vec3{0.0f, 0.0f, -200.0f});
 
-    float currentwidth = -1;
-    float currentheight = -1;
+    Shader shader{proj, view};
 
-    int rasterpos[4];
-    glRasterPos4f(-1.f,-1.f,0.f,1.f);
-    glGetIntegerv(GL_CURRENT_RASTER_POSITION, rasterpos);
 
-    std::cout << "Raster Pos: " << rasterpos[0] << ", " << rasterpos[1] << ", " << rasterpos[2] << ", " << rasterpos[3] << '\n';
+    Triangle JASH{
+     glm::vec3 { 50.0f,  50.0f,  0.0f },
+     glm::vec3 { 250.0f, 150.0f, 0.0f },
+     glm::vec3 { 450.0f, 50.0f,  0.0f },
+     yellow
+    };
+
+    Triangle SEB{
+     glm::vec3 { 5.0f,  5.0f,   0.0f },
+     glm::vec3 { 50.0f, 300.0f, 0.0f },
+     glm::vec3 { 37.0f, 12.0f,  0.0f },
+     green
+    };
+
+    Triangle TOMMY{
+     glm::vec3 { 3.0f, 1.0f , 0.0f },
+     glm::vec3 { 6.0f, 9.0f , 0.0f },
+     glm::vec3 { 4.0f, 20.0f, 0.0f },
+     burgundy
+    };
+
+
+    glm::mat4 model { 1.0f };
+
+    float rotation = 0;
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        frameBuff.Clear();
 
         //input
         glfwPollEvents();
 
+        rotation += 0.005f;
+        model = glm::rotate(glm::mat4{1.0f}, rotation, glm::vec3(1.0f, 0.0f, 0.0f));
+
         //render
-        glDrawPixels(500, 500, GL_RGB, GL_FLOAT, pixlrgb.data());
+        frameBuff.DrawTriangle(shader.RasterizeTriangle(JASH, model));
+        frameBuff.DrawTriangle(shader.RasterizeTriangle(SEB, model));
+        frameBuff.DrawTriangle(shader.RasterizeTriangle(TOMMY, model));
+
+
+        glDrawPixels(WIDTH, HEIGHT, GL_RGBA, GL_FLOAT, frameBuff.Colours.data());
         
         glfwSwapBuffers(window);
     }
